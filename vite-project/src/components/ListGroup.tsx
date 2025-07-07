857ac27bad622f705801a56c0c42017826d69a47
Kelia Iradukunda <iradukundakelia@gmail.com>
2025-07-07 18:48:40 +0200

    Conditional rendering

--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -1,20 +1,18 @@
 function ListGroup() {
-  const items = [
-    'NewYork',
-    'Uganda',
-    'Rwanda',
-    'Kenya',
-  ];
+  let items = ["NewYork", "Uganda", "Rwanda", "Kenya"];
+  items = []; 
 
-  
-  return (
-    <>
-      <h1>List</h1>
-      <ul className="list-group">
-       {items.map(item => <li key={item}>{item}</li>)}
-      </ul>
-    </>
-  );
+    return (
+      <>
+        <h1>List</h1>
+        {items.length === 0 && <p>No item found</p>}
+        <ul className="list-group">
+          {items.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </>
+    );
 }
 
 export default ListGroup;
