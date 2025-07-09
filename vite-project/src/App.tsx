5a808c4c4111e152312dcf53aaa57c740b220b3e
Kelia Iradukunda <iradukundakelia@gmail.com>
2025-07-09 21:40:40 +0200

    Passing data via props

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,9 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup /></div>
+ let items = ["NewYork", "Uganda", "Rwanda", "Kenya"];
+
+  return <div><ListGroup items={items} heading="Cities"/></div>
 }
 
 export default App;
--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -1,18 +1,38 @@
-function ListGroup() {
-  let items = ["NewYork", "Uganda", "Rwanda", "Kenya"];
-  items = []; 
+import { useState } from "react";
 
-    return (
-      <>
-        <h1>List</h1>
-        {items.length === 0 && <p>No item found</p>}
-        <ul className="list-group">
-          {items.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </>
-    );
+
+//{items: [], heading: string}
+interface Props{
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({items, heading}: Props) {
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
