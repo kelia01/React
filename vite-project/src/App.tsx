110661d98563808c321eec767b3c08aebf4eb50e
Kelia Iradukunda <iradukundakelia@gmail.com>
2025-07-14 09:25:30 +0200

    Updating objects

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,16 +4,18 @@ import Message from "./components/Message";
 
 function App() {
 
-  const [person, setPerson] = useState({
-    firstName: '',
-    lastName: ''
-  })
+  const [drink, setDrink] = useState({
+    title: 'Americano',
+    price: 5
+  });
 
+  const handleClick = () => {
+    setDrink({... drink, price: 6});
+  }
   return (
   <div>
-   <Message />
-   <Message />
-   <Message />
+    {drink.price}
+   <button onClick={handleClick}>click me</button>
   </div>
 )
 }
