066ce61422cd45eb0907fd84181c1002124b9f78
Kelia Iradukunda <iradukundakelia@gmail.com>
2025-07-15 09:48:30 +0200

    Start of revision exercises using tailwind css and finish exercise 1

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,28 +1,8 @@
-
-import { useState } from "react";
-import Message from "./components/Message";
-import produce from "immer";
+import Hello from "./components/ReactExercises/HelloWorld";
 
 function App() {
-
-  const [bugs, setBugs] = useState([
-    {bugg:'components',id: 1, state: false},
-    {bugg:'message', id:2, state: false},
-  ]);
-
-  const handleClick = () => {
-    // setBugs(bugs.map(bug => bug.id === 1 ? {...bug, state: true} : bug));
-
-    setBugs(produce(draft => {
-       const bug = draft.find(bug => bug.id === 1);
-       if(bug) bug.state = true;
-    }))
-  }
   return (
-  <div>
-    {bugs.map(bug => <p key={bug.id}>{bug.state} {bug.state ? 'Fixed' : 'New'}</p>)}
-   <button onClick={handleClick}>click me</button>
-  </div>
+    <Hello />
 )
 }
 
---
+++ vite-project/src/components/ReactExercises/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div className='bg-amber-400 w-96 h-96 flex justify-center items-center'>
+      <p className='text-center'>Hello, World!</p>
+      </div>
+  )
+}
+
+export default Hello
