5cb392df009e8106e875887f780e2e01c78287da
Kelia Iradukunda <iradukundakelia@gmail.com>
2025-07-21 14:28:42 +0200

    Exercise 4 about clicking and counting the number u did

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,9 +4,7 @@ import Counter from "./components/ReactExercises/Counter";
 function App() {
   return (
     <>
-      <Hello>1</Hello>
-      <Hello> 2</Hello>
-      <Hello> 3</Hello>
+      <Counter />
     </>
   );
 }
--- vite-project/src/components/ReactExercises/Counter.tsx
+++ vite-project/src/components/ReactExercises/Counter.tsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-
+import React, { useState } from 'react'
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+
+  const [status, setStatus] = useState(0);
+
   const handleClick = () => {
-      //id === 'add' ? setCount(count+1) : setCount(count-1)
+    setStatus(status + 1);
   }
   return (
-    <div className='flex justify-center items-center space-x-4'>
-      <p>{count}</p>
-        <button id = 'add' className='bg-yellow-300 rounded-full text-black w-20 h-20' onClick={handleClick}> - </button>
-        <button id = 'minus' className='bg-yellow-300 rounded-full text-black w-20 h-20' onClick={handleClick}> + </button>
+    <div>
+      <p>Clicked {status}</p>
+      <button className='bg-amber-600 text-black text-lg' onClick={handleClick}>Click Me</button>
     </div>
   )
 }
